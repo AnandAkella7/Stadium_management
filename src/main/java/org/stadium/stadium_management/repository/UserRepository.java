75bb62498b09f70db984f524de55f2ac25c89598
Yegna Sai Anand <anandakella777@gmail.com>
2025-01-30 21:19:06 -0800

    Added Ticket, User Repositories

--- src/main/java/org/stadium/stadium_management/repository/UserRepository.java
+++ src/main/java/org/stadium/stadium_management/repository/UserRepository.java
@@ -4,7 +4,30 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.stadium.stadium_management.model.User;
 
+import java.util.Optional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // HashMap cache for quick lookups 
+    Map<Long, User> userCache = new HashMap<>();
+
+    Optional<User> findByEmail(String email);
+
+    default Optional<User> findCacheUser(Long id)
+    {
+        return Optional.ofNullable(userCache.get(id));
+    }
+
+    default void cacheUser(User user)
+    {
+        userCache.put(user.getId(), user);
+    }
 }
 
