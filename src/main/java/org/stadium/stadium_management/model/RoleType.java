c2ba3308546066ff1f220eaede0fc7b2afe567da
Yegna Sai Anand <anandakella777@gmail.com>
2025-02-28 18:27:17 -0800

    Created Modules

--- src/main/java/org/stadium/stadium_management/model/RoleType.java
+++
@@ -1,5 +0,0 @@
-package org.stadium.stadium_management.model;
-
-public enum RoleType {
-    ADMIN, STAFF, USER
-}
--- src/main/java/org/stadium/stadium_management/model/TicketType.java
+++
@@ -1,9 +0,0 @@
-package org.stadium.stadium_management.model;
-
-public enum TicketType {
-    ADULT,
-    CHILDREN,
-    SENIOR_CITIZEN,
-    VIP,
-    REGULAR
-}
