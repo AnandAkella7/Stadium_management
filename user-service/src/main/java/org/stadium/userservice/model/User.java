d95b89996ab760f4c235da11aec8f12945e38268
Yegna Sai Anand <anandakella777@gmail.com>
2025-02-28 19:30:45 -0800

    User entity created

---
+++ user-service/src/main/java/org/stadium/userservice/model/User.java
@@ -0,0 +1,45 @@
+package org.stadium.userservice.model;
+
+import jakarta.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table( name ="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column( unique = true, nullable = false)
+    @NotBlank(message =" Username is required")
+    private String Username;
+
+
+    @Column( unique = true, nullable = false)
+    @Email(message = "Invalid email format")
+    @NotBlank(message =" Email is required")
+    private String email;
+
+    @JsonIgnore
+    @Column( unique = true, nullable = false)
+    private String password;
+
+    @Column(name ="registered_DT", nullable = false)
+    private LocalDateTime registeredDateTime;
+
+}
+
+
